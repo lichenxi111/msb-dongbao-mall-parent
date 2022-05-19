af87d822016cb3a195c69d8b7416b9a67c06a632
lichenxi111 <1430728374@qq.com>
2022-05-19 12:14:23 +0800

    Create MsbDongbaoMallParentV1Application.java

---
+++ msb-dongbao-mall-parent-v1/src/main/java/com/msb/MsbDongbaoMallParentV1Application.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbDongbaoMallParentV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsbDongbaoMallParentV1Application.class, args);
+    }
+
+}
